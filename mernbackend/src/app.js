c83f0bb359190fefdbda24be2fbb6797c66d57ee
abdul-sohail24 <abdulsohail22246@gmail.com>
2023-02-26 20:19:04 +0530

    Linked the Connections

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+require("./db/conn");
 
 const port = process.env.PORT || 3000;
 
 //Home Page
 app.get("/", (req, res) => {
-    res.send('Hey')
+    res.send(`Hey`)
 });
 
 app.listen(port, () => {
