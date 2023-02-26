08ab8489c640bdc0ae6a1fd978d78e15e7b6aa86
abdul-sohail24 <abdulsohail22246@gmail.com>
2023-02-26 20:28:30 +0530

    Link app.js & index.html

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 require("./db/conn");
 
 const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public");
+
+//Linking app.js & index.html
+app.use(express.static(static_path))
 
 //Home Page
 app.get("/", (req, res) => {
