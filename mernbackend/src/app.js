154ca95c6bba18823ea526229c1729d29b2e2741
abdul-sohail24 <abdulsohail22246@gmail.com>
2023-03-12 19:18:26 +0530

    Add hbs code

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
+const hbs = require("hbs")
+
 require("./db/conn");
 
 const port = process.env.PORT || 3000;
 
 const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
 //Linking app.js & index.html
 app.use(express.static(static_path));
 
 //Setting view Engine (get data from index.hbs)
 app.set("view engine", "hbs");
+
+//Setting views (get data from views)
+app.set("views", template_path);
+
+//Setting hbs
+hbs.registerPartials(partials_path)
 
 //Home Page
 app.get("/", (req, res) => {
