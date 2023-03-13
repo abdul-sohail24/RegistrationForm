f4d88dfcb0d3c2d3aaa9040ea4fa80ff2a7f0017
abdul-sohail24 <abdulsohail22246@gmail.com>
2023-03-13 23:55:00 +0530

    Add POST Method

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -5,12 +5,16 @@ const path = require("path");
 const hbs = require("hbs")
 
 require("./db/conn");
+const Register = require("./models/registers")
 
 const port = process.env.PORT || 3000;
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 //Linking app.js & index.html
 app.use(express.static(static_path));
@@ -31,6 +35,17 @@ app.get("/", (req, res) => {
 
 app.get("/register", (req, res) => {
     res.render("register");
+})
+
+//Create a New User in DB
+app.post("/register", async (req, res) => {
+    try {
+        console.log(req.body.firstname);
+        res.send(req.body.firstname);
+    }
+    catch(e) {
+        res.status(400).send(e);
+    }
 })
 
 app.listen(port, () => {
